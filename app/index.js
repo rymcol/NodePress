2178611e8050a4bb697233d0ea7c3f93be6c87ee
Ryan Collins <ryan@ryanmcollins.com>
2016-08-08 11:50:14 -0400

    Added basic file structure
    Added start point, application directory, installed express, setup basic hello world for first test

---
+++ app/index.js
@@ -0,0 +1,17 @@
+// app/index.js
+
+const express = require('express')  
+const app = express()  
+const port = 8585
+
+app.get('/', (request, response) => {  
+  response.send('Hello from Express!')
+})
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
