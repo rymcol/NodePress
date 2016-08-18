552e02930f7a02fb494e5da4050768278332ff19
Ryan Collins <ryan@ryanmcollins.com>
2016-08-18 12:30:44 -0400

    Removed Mustache (Handlebars)

--- app/index.js
+++ app/index.js
@@ -1,33 +1,36 @@
 // app/index.js
- 
-const express = require('express')  
-const exphbs = require('express-handlebars')
-const app = express()  
+
+const express = require('express')
+const app = express()
 const port = 8585
-const host = "169.254.237.101"
+const host = "0.0.0.0" //"169.254.237.101"
 
 const indexHandler = require('./indexHandler')
 const blogHandler = require('./blogHandler')
-
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+const commondHandler = require('./CommonHandler')
 
 /* Serve Static Files */
 app.use(express.static('public'));
 
-app.get('/', (request, response) => {  
-	var indexContent = indexHandler.makeIndexContent()
-	response.render('index', indexContent)
+app.get('/', (request, response) => {
+    const header = commondHandler.makeHeader();
+	const indexContent = indexHandler.makeIndexContent();
+    const footer = commondHandler.makeFooter();
+    const page = header + indexContent + footer;
+
+	response.send(page);
 })
 
-app.get('/blog', (request, response) => {  
-	var blogContent = blogHandler.makeBlogContent()
-	response.render('blog', {
-  	  content: blogContent
-  })
+app.get('/blog', (request, response) => {
+    const header = commondHandler.makeHeader();
+	const blogContent = blogHandler.makeBlogContent();
+    const footer = commondHandler.makeFooter();
+    const page = header + blogContent + footer;
+
+	response.send(page);
 })
 
-app.listen(port, host, (err) => {  
+app.listen(port, host, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
